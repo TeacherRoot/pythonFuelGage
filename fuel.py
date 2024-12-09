e8cc5d8450c1989e986741564c6b60a633e6ab81
Ann Root <ann.root@bvsd.org>
2024-12-09 11:54:29 -0700

    Create fuel.py

---
+++ fuel.py
@@ -0,0 +1,8 @@
+fuel = input("Fraction: ")
+
+#wow
+sp = fuel.rsplit("/")  # this is now a list with 2 items in it
+print(sp)
+
+# Need to add code to divide the values in the list 
+# then print out the result
